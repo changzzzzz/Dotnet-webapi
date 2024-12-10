b0f99f7fa89ddd79cc1d90ba42a80b07a2acf945
Yang Liu <youngliu0614@gmail.com>
2024-12-10 21:50:11 +1100

    aspnet core webapi 6

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController: ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll(){
+            var stocks = _context.Stock.ToList()
+            .Select(s=>s.ToStockDto());//tolist is necessary: urther excuation
+            
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stock.Find(id);
+            if(stock == null){
+                return NotFound();
+            }
+            return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stockModel = stockDto.ToStockFromCreateDTO();
+            _context.Stock.Add(stockModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new {id = stockModel.Id}, stockModel.ToStockDto());
+        }
+    }
+}
